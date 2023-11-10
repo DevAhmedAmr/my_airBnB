e51bedc9754a60495e3f827fec809852b1fbbd92
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-10 20:24:01 +0200

    first commit (A.C)

--- models/base_model.py
+++ models/base_model.py
@@ -6,28 +6,28 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id: str = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
+        self.created_at: datetime = datetime.now()
         self.name = None
         self.my_number = None
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at: datetime = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         class_attr = self.__dict__
         filtered_class_attr = dict()
 
-        filtered_class_attr["updated_at"]: str = self.updated_at
-        filtered_class_attr["created_at"]: str = self.created_at
-
         for key in class_attr:
             if class_attr[key] is not None:
                 filtered_class_attr[key] = class_attr[key]
         filtered_class_attr["__class__"] = self.__class__.__name__
+
+        filtered_class_attr["updated_at"]: str = str(self.updated_at.isoformat())
+        filtered_class_attr["created_at"]: str = str(self.created_at.isoformat())
 
         return filtered_class_attr
 
