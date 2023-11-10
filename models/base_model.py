aa1d0d10e14cf72e8f9016b96b0d375e6a688783
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-10 20:05:20 +0200

    first commit (A.C)

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,46 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id: str = str(uuid.uuid4())
+        self.created_at = datetime.now().isoformat()
         self.name = None
         self.my_number = None
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
-        return "[{}] ({}) created_at: {} updated_at: {} {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.created_at.isoformat(),
-            self.updated_at.isoformat(),
-            self.__dict__,
-        )
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
         class_attr = self.__dict__
-        filtered_class_attr = {}
+        filtered_class_attr = dict()
 
-        # Convert created_at and updated_at to ISO format strings
-        filtered_class_attr["created_at"] = self.created_at.isoformat()
-        filtered_class_attr["updated_at"] = self.updated_at.isoformat()
+        filtered_class_attr["updated_at"]: str = self.updated_at
+        filtered_class_attr["created_at"]: str = self.created_at
 
-        # Include only non-None values and non-default values
-        for key, value in class_attr.items():
-            if value is not None and value != "default_value":
-                filtered_class_attr[key] = value
-
-        # Add the __class__ key with the class name
+        for key in class_attr:
+            if class_attr[key] is not None:
+                filtered_class_attr[key] = class_attr[key]
         filtered_class_attr["__class__"] = self.__class__.__name__
 
         return filtered_class_attr
 
 
-# Example usage:
-base_model_instance = BaseModel()
-print(str(base_model_instance))
-result_dict = base_model_instance.to_dict()
-print(result_dict)
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+# print(my_model)
+print("***** \n")
+my_model.save()
+# print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
